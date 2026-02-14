89d181d0dc3191079cc4116db2c5cb7424e1a4a8
Johannes Förstner <johannes@famfoerstner.de>
2026-02-14 18:10:21 +0100

    Run in Vanilla

--- run.bat
+++ run.bat
@@ -12,7 +12,7 @@ call "%bits%\build.bat" %*
 ::pause
 
 :: Run it!
-"%ds%\DSLOA.exe" nointro=true map=%map%
+"%ds%\DungeonSiege.exe" nointro=true map=%map%
 
 :: Cleanup resources so as not to confuse Siege Editor
 call "%bits%\cleanup.bat" %*
