9ce48001df086ccb21632092913bd6ae399935f2
Johannes Förstner <johannes@famfoerstner.de>
2026-02-15 10:02:40 +0100

    Cool hack using powershell: patch the prio byte to Factory!

--- build.bat
+++ build.bat
@@ -24,6 +24,9 @@ robocopy "%bits%\sound" "%tmp%\Bits\sound" /S
 robocopy "%bits%\world\contentdb\components" "%tmp%\Bits\world\contentdb\components" /S /xf *.lqd20
 "%tc%\RTC.exe" -source "%tmp%\Bits" -out "%ds%\Resources\%mod_cs%.dsres" -copyright "%copyright%" -title "%title%" -author "%author%"
 if %errorlevel% neq 0 pause
+:: patch base prio (0x00 = Factory, 0x10 = Language, 0x20 = Expansion, 0x30 = Patch, 0x40 = User)
+powershell -Command "& {$bytes = [System.IO.File]::ReadAllBytes('%ds%\Resources\%mod_cs%.dsres'); $bytes[0x35] = 0x00; [System.IO.File]::WriteAllBytes('%ds%\Resources\%mod_cs%.dsres', $bytes);}"
+if %errorlevel% neq 0 pause
 
 :: Compile demo map file
 rmdir /S /Q "%tmp%\Bits"
